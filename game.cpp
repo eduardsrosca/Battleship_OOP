39c91fabb8370c14a28f9985b5b8e702643f1df8
eduardsrosca <135509876+eduardsrosca@users.noreply.github.com>
2025-01-09 12:27:50 +0200

    game.cpp

--- game.cpp
+++ game.cpp
@@ -2,6 +2,8 @@
 #include "exceptions.h"
 #include "clearscreen.h"
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 Game::Game(std::string name1, std::string name2)
     : player1(std::move(name1)), player2(std::move(name2)) {}
@@ -20,14 +22,53 @@ void Game::start() {
     while (!gameOver) {
         try {
             gameOver = currentPlayer->takeTurn(*opponent);
-            if (!gameOver) {
-                std::swap(currentPlayer, opponent);
-            }
-        } catch (const GameException& e) {
-            std::cerr << e.what() << std::endl;
-            return;
+            clearScreen();
+            std::swap(currentPlayer, opponent);
+        } catch (const std::exception& e) {
+            std::cout << e.what() << "\n";
         }
     }
+}
 
-    std::cout << currentPlayer->getName() << " wins!\n";
+TimedGame::TimedGame(std::string name1, std::string name2, int timeLimit)
+    : Game(std::move(name1), std::move(name2)), timeLimit(timeLimit) {}
+
+void TimedGame::start() {
+    clearScreen();
+    player1.placeShips();
+    clearScreen();
+    player2.placeShips();
+    clearScreen();
+
+    int player1Time = timeLimit;
+    int player2Time = timeLimit;
+
+    bool gameOver = false;
+    Player* currentPlayer = &player1;
+    Player* opponent = &player2;
+    int* currentTime = &player1Time;
+    int* opponentTime = &player2Time;
+
+    while (!gameOver) {
+        auto startTurn = std::chrono::steady_clock::now();
+
+        try {
+            std::cout << currentPlayer->getName() << "'s turn. Remaining time: " << *currentTime << " seconds.\n";
+            gameOver = currentPlayer->takeTurn(*opponent);
+            clearScreen();
+        } catch (const std::exception& e) {
+            std::cout << e.what() << "\n";
+        }
+
+        auto endTurn = std::chrono::steady_clock::now();
+        *currentTime -= std::chrono::duration_cast<std::chrono::seconds>(endTurn - startTurn).count();
+
+        if (*currentTime <= 0) {
+            std::cout << currentPlayer->getName() << " ran out of time! " << opponent->getName() << " wins!\n";
+            break;
+        }
+
+        std::swap(currentPlayer, opponent);
+        std::swap(currentTime, opponentTime);
+    }
 }
