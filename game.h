e80c9e8ec91019ad0d7ad3eef821116631b6c29a
eduardsrosca <135509876+eduardsrosca@users.noreply.github.com>
2025-01-09 12:27:14 +0200

    game.h

--- game.h
+++ game.h
@@ -2,15 +2,18 @@
 #define GAME_H
 
 #include "player.h"
+#include <string>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <utility>
 
 /**
  * @class Game
  * @brief Manages the overall game flow.
  */
-
-
 class Game {
-private:
+protected:
     Player player1;
     Player player2;
 
@@ -20,8 +23,26 @@ public:
     /**
      * @brief Starts the game, including ship placement and game loop.
      */
+    virtual void start(); // virtual for polymorphism
 
-    void start();
+    virtual ~Game() = default;
+};
+
+/**
+ * @class TimedGame
+ * @brief Derived class that adds a timer for each player.
+ */
+class TimedGame : public Game {
+private:
+    int timeLimit;
+
+public:
+    TimedGame(std::string name1, std::string name2, int timeLimit);
+
+    /**
+     * @brief Starts the timed game.
+     */
+    void start() override;
 };
 
 #endif // GAME_H
